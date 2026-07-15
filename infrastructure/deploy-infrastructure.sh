d3f1e485f30f6cfd7fcaa4692bca8d89abda16f0
Leonardo Seminara <jlseminara@gmail.com>
2026-07-15 11:38:27 -0300

    feat: improved shell scripts

--- infrastructure/deploy-infrastructure.sh
+++ infrastructure/deploy-infrastructure.sh
@@ -1,35 +1,35 @@
-cd redis
+cd 01.Redis
 ./deploy-redis.sh
 cd ..
 
-cd apigateway
-./deploy-tyk.sh
-cd ..
-
-cd service-registry
-./deploy-consul.sh
-cd ..
-
-cd configuration-service
-./deploy-configserver.sh
-cd ..
-
-cd database-cluster
+cd 02.Database-cluster
 ./deploy-postgres.sh
 cd ..
 
-cd keycloak
+cd 03.Keycloak
 ./deploy-keycloak.sh
 cd ..
 
-cd message-broker
+cd 04.Service-registry
+./deploy-consul.sh
+cd ..
+
+cd 05.Configuration-service
+./deploy-configserver.sh
+cd ..
+
+cd 06.Apigateway
+./deploy-tyk.sh
+cd ..
+
+cd 07.Message-broker
 ./deploy-rabbit.sh
 cd ..
 
-cd mongodb
+cd 08.Mongodb
 ./deploy-mongodb.sh
 cd ..
 
-cd observability
+cd 09.Observability
 ./depploy-grafana.sh
 cd ..
--- infrastructure/remove-infrastructure.sh
+++ infrastructure/remove-infrastructure.sh
@@ -1,37 +1,38 @@
-cd redis
+cd 01.Redis
 ./remove-redis.sh
 cd ..
 
-cd apigateway
-./remove-tyk.sh
-cd ..
-
-cd service-registry
-./remove-consul.sh
-cd ..
-
-cd configuration-service
-./remove-configserver.sh
-cd ..
-
-cd database-cluster
+cd 02.Database-cluster
 ./remove-postgres.sh
 cd ..
 
-cd keycloak
+cd 03.Keycloak
 ./remove-keycloak.sh
 cd ..
 
-cd message-broker
+cd 04.Service-registry
+./remove-consul.sh
+cd ..
+
+cd 05.Configuration-service
+./remove-configserver.sh
+cd ..
+
+cd 06.Apigateway
+./remove-tyk.sh
+cd ..
+
+cd 07.Message-broker
 ./remove-rabbit.sh
 cd ..
 
-cd mongodbf
+cd 08.Mongodb
 ./remove-mongodb.sh
 cd ..
 
-cd observability
+cd 09.Observability
 ./remove-grafana.sh
 cd ..
+
 
 kubectl delete pvc --all
